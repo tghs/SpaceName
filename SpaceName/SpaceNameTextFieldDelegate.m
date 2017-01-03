005dd25b4c989d1b9e4d418cf1b6474c3a5a84cb
Tim Sheridan <tghs@tghs.net>
2017-01-03 21:18:06 +0000

    Let escape abort space name editing

--- SpaceName/SpaceNameTextFieldDelegate.m
+++ SpaceName/SpaceNameTextFieldDelegate.m
@@ -11,7 +11,11 @@
 @implementation SpaceNameTextFieldDelegate
 
 - (BOOL)control:(NSControl *)control textView:(NSTextView *)textView doCommandBySelector:(SEL)commandSelector {
-	if (commandSelector == @selector(complete:)) {
+	if ((commandSelector == @selector(complete:)) || // Clicking outside of app to abort
+	    (commandSelector == @selector(cancel:))) {   // Pressing escape to abort
+		[self.appDelegate cancelEdit];
+		return YES;
+	} else if (commandSelector == @selector(cancel:)) {
 		[self.appDelegate cancelEdit];
 		return YES;
 	}
